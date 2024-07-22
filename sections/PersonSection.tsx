7529261a9436c1c259936285a6f638e0cf250136
Arthur Silva de Carvalho Azevedo <arthur.c.azevedo@gmail.com>
2024-07-22 07:06:03 -0300

    de novo...

--- sections/PersonSection.tsx
+++ sections/PersonSection.tsx
@@ -1,4 +1,4 @@
-import { ResultSet } from "https://esm.sh/@libsql/client@0.6.0/web";
+import { ResultSet, Rows } from "https://esm.sh/@libsql/client@0.6.0/web";
 
 interface Props {
   /**
@@ -12,7 +12,7 @@ interface Props {
    * @description profiles.
    * @default 
    */
-  resultSet: Promise<ResultSet>;
+  resultSet: ResultSet;
 }
 
 export default function Section(props: Props) {
@@ -22,7 +22,7 @@ export default function Section(props: Props) {
       id="Person"
       class="container py-10 flex flex-col h-screen w-full items-center justify-center gap-16"
     >
-      <p>{props.resultSet}</p>
+      <p>{props.resultSet.rows.map((row:Rows) => {row})}</p>
     </div>
   );
 }
